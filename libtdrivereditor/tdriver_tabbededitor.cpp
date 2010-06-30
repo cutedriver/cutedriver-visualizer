ebe591fcdcbfb41f9d38d735ed8a72285b7fccdf
Ari Hyttinen <ext-ari.hyttinen@nokia.com>
2010-06-30 17:59:41 +0300

    Compiler warning cleanup

--- libtdrivereditor/tdriver_tabbededitor.cpp
+++ libtdrivereditor/tdriver_tabbededitor.cpp
@@ -1,21 +1,21 @@
-/*************************************************************************** 
-** 
-** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies). 
-** All rights reserved. 
-** Contact: Nokia Corporation (testabilitydriver@nokia.com) 
-** 
-** This file is part of Testability Driver. 
-** 
-** If you have questions regarding the use of this file, please contact 
-** Nokia at testabilitydriver@nokia.com . 
-** 
-** This library is free software; you can redistribute it and/or 
-** modify it under the terms of the GNU Lesser General Public 
-** License version 2.1 as published by the Free Software Foundation 
-** and appearing in the file LICENSE.LGPL included in the packaging 
-** of this file. 
-** 
-****************************************************************************/ 
+/***************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (testabilitydriver@nokia.com)
+**
+** This file is part of Testability Driver.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at testabilitydriver@nokia.com .
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
 
 
 #include "tdriver_tabbededitor.h"
@@ -156,6 +156,7 @@ void TDriverTabbedEditor::connectTabSignals(TDriverCodeTextEdit *editor)
 
 void TDriverTabbedEditor::currentChangeAction(int index)
 {
+    Q_UNUSED(index);
     //qDebug() << FFL << "Tab changed to" << index;
     disconnectTabSignals();
     TDriverCodeTextEdit *editor = qobject_cast<TDriverCodeTextEdit*>(currentWidget());
