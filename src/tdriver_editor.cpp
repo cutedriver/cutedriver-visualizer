ba10f091f17223d1c0571f93986fb1c04cfcdb0f
Ari Hyttinen <ext-ari.hyttinen@nokia.com>
2010-12-15 17:02:41 +0200

    Removed warning.

--- src/tdriver_editor.cpp
+++ src/tdriver_editor.cpp
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
 
     debugOutFile->open( QIODevice::WriteOnly | QIODevice::Text );
     qInstallMsgHandler(output);
-    qDebug(qPrintable("Log opened: " + QDateTime::currentDateTime().toString()));
+    qDebug("%s", qPrintable("Log opened: " + QDateTime::currentDateTime().toString()));
 
     qRegisterMetaType<BAList>("BAList");
     qRegisterMetaType<BAListMap>("BAListMap");
