4572ce60c55a05177c0e3dcc8b9dcd7fc3b91db2
Ari Hyttinen <ext-ari.hyttinen@nokia.com>
2011-04-08 15:49:00 +0300

    Now install_tdriver_interface.bat makes directories and copies also templates and completions.

--- install_tdriver_interface.bat
+++ install_tdriver_interface.bat
@@ -1,1 +1,8 @@
+mkdir C:\tdriver\visualizer
 ruby -cw libtdriverutil\tdriver_interface.rb && copy /y libtdriverutil\tdriver_interface.rb C:\tdriver\visualizer\tdriver_interface.rb
+
+mkdir C:\tdriver\visualizer\templates
+copy libtdrivereditor\templates\*.* C:\tdriver\visualizer\templates
+
+mkdir C:\tdriver\visualizer\completions
+copy libtdrivereditor\completions\*.* C:\tdriver\visualizer\completions
